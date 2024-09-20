d03dfe2a73a22325a6c3eb2ec2eb4dc9ab7a7c89
swapnil <swapnil.zakade.33@gmail.com>
2024-09-21 01:27:01 +0530

    added update user controller.

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -58,3 +58,64 @@ export const selectUser = async (req, res) => {
     return res.status(500).json({ error: error.message });
   }
 };
+
+export const updateUser = async (req, res) => {
+  const { name, email, about, currentPassword, newPassword } = req.body;
+  let { profileImg } = req.body;
+
+  const userId = req.user._id;
+  try {
+    let user = await User.findOne(userId);
+    if (!user) return res.status(404).json({ message: "User not found." });
+
+    if (
+      (!newPassword && currentPassword) ||
+      (!currentPassword && newPassword)
+    ) {
+      return res.status(400).json({
+        message: "Please provide both current password and new password.",
+      });
+    }
+
+    if (currentPassword && newPassword) {
+      const isMatch = await bcrypt.compare(currentPassword, user.password);
+      if (!isMatch)
+        return res
+          .status(400)
+          .json({ message: "current password is incorrect" });
+
+      if (newPassword.length < 6)
+        return res
+          .status(400)
+          .json({ message: "Password must be atleast 6 characters long." });
+
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(newPassword, salt);
+    }
+
+    if (profileImg) {
+      if (user.profileImg) {
+        await cloudanary.uploader.destroy(
+          user.profileImg.split("/").pop().split(".")[0]
+        );
+      }
+
+      const uploadedResponse = await cloudanary.uploader.upload(profileImg);
+      profileImg = uploadedResponse.secure_url;
+    }
+
+    user.name = name || user.name;
+    user.email = email || user.email;
+    user.about = about || user.about;
+    user.profileImg = profileImg || user.profileImg;
+
+    user = await user.save();
+
+    user.password = null;
+
+    return res.status(200).json({ user });
+  } catch (error) {
+    console.log("Error in update User", error.message);
+    return res.status(500).json({ error: error.message });
+  }
+};
--- models/user.model.js
+++ models/user.model.js
@@ -22,8 +22,7 @@ const userSchema = new mongoose.Schema(
     },
     profileImg: {
       type: String,
-      default:
-        "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg",
+      default: "",
     },
   },
   { timestamps: true }
